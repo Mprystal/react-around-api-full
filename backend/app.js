c29c4c40096fb290edb49d6018c85a2e585bade1
Mprystal <mjprystal@gmail.com>
2021-02-26 20:38:52 -0500

    update app.js backend header

--- backend/app.js
+++ backend/app.js
@@ -30,7 +30,6 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://aroundtheusa.students.nomoreparties.site');
   res.header('Access-Control-Allow-Origin', 'http://api.aroundtheusa.students.nomoreparties.site');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
   next();
 });
 

f5632fe1c23a91b406a8e3a93252203a11defddd
Mprystal <mjprystal@gmail.com>
2021-02-27 11:51:05 -0500

    update cors backend

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,10 @@ const { requestLogger, errorLogger } = require('./middleware/logger');
 const { PORT = 3000 } = process.env;
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: 'http://aroundtheusa.students.nomoreparties.site',
+  allowedHeaders: ['Origin', 'Content-Type', 'access-control-allow-origin'],
+}));
 
 const userRouter = require('./routes/users');
 const cardRouter = require('./routes/cards');

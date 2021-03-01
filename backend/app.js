9515bd3e4a3d7a7126cef6d31afd455d47dd0c4f
Mprystal <mjprystal@gmail.com>
2021-03-01 11:03:03 -0500

    update cors backend

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-// const cors = require('cors');
+const cors = require('cors');
 const path = require('path');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');

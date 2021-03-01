20bba3a96df3d23e596d079588c179bcb7710638
Mprystal <mjprystal@gmail.com>
2021-03-01 12:03:50 -0500

    update cors backend

--- backend/app.js
+++ backend/app.js
@@ -30,6 +30,11 @@ mongoose.connect('mongodb://localhost:27017/aroundb', {
 //   allowedHeaders: ['Origin', 'Content-Type', 'Access-Control-Allow-Origin'],
 // }));
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
 app.use(cors());
 
 app.use(helmet());

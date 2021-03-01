62115fcfc0671f9898f719822a328f4080c77c22
Mprystal <mjprystal@gmail.com>
2021-03-01 16:58:09 -0500

    update cors backend

--- backend/app.js
+++ backend/app.js
@@ -30,8 +30,10 @@ mongoose.connect('mongodb://localhost:27017/aroundb', {
 //   allowedHeaders: ['Origin', 'Content-Type', 'Access-Control-Allow-Origin'],
 // }));
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'https://aroundtheusa.students.nomoreparties.site');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
   next();
 });
 

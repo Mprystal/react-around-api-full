e378c1fc65b9a3bf7636f8525025db736f4d050e
Mprystal <mjprystal@gmail.com>
2021-03-02 11:03:44 -0500

    update backend users.js controller

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -13,7 +13,7 @@ const getUsers = (req, res) => {
 };
 
 const getUser = (req, res, next) => {
-  const { token } = req.get("Authorization");
+  const { token } = req.headers("Authorization");
   const decodedUser = jwt.verify(token, JWT_SECRET)
 
   User.findById(decodedUser).then((user) => {

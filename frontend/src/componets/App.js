5abff912654fac38c7a9bf4e16566729d6a85cf2
Mprystal <mjprystal@gmail.com>
2021-03-02 14:35:05 -0500

    update frontend app.js

--- frontend/src/componets/App.js
+++ frontend/src/componets/App.js
@@ -69,7 +69,7 @@ function App() {
 
   function handleCardLike(card) {        
     const isLiked = card.likes.some(i => i._id === currentUser._id);
-    console.log(isLiked,currentUser)
+    console.log(card)
 
     // api.changeLikeCardStatus(card._id, isLiked, token).then((newCard) => {
     //   console.log(newCard)

1cda03ac33a21f6c6b93f257d6dc0958fdcfb335
Mprystal <mjprystal@gmail.com>
2021-03-02 14:18:18 -0500

    update frontend app.js

--- frontend/src/componets/App.js
+++ frontend/src/componets/App.js
@@ -53,9 +53,9 @@ function App() {
   React.useEffect(() => {
     Promise.all([api.getUserInfo(token),api.getCardList(token)]).then(
       ([userInfo,cardListData]) => { 
-        console.log(userInfo.user, cardListData[0])
+        console.log(userInfo.user, cardListData)
          setCurrentUser(userInfo.user)
-         setCards(cardListData[0])
+         setCards(cardListData)
         }).catch((err) => {
           console.log(`Error: ${err}`);
         })

af606af64d7cb57dd74d588e81f97e801df2bb0a
Mprystal <mjprystal@gmail.com>
2021-03-03 09:55:59 -0500

    update frontend app.js

--- frontend/src/componets/Card.js
+++ frontend/src/componets/Card.js
@@ -11,7 +11,7 @@ function Card(props){
   ); 
 
     // Check if the card was liked by the current user
-  const isLiked = props.card.likes.some(i => i._id === currentUser._id);
+  const isLiked = props.card.likes.some(i => i === currentUser._id);
 
   // Create a variable which you then set in `className` for the like button
   const cardLikeButton = `${isLiked ? 'element__card-heart_active' : 'element__card-heart'}`; 

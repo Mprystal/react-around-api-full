f9013b0315eae4eb6f53d8e0e91e11888fbaf11e
Mprystal <mjprystal@gmail.com>
2021-03-05 20:18:04 -0500

    cors problems

--- backend/app.js
+++ backend/app.js
@@ -26,8 +26,22 @@ mongoose.connect('mongodb://localhost:27017/aroundb', {
   useFindAndModify: false,
 });
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'https://around.nomoreparties.co');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+  next();
+});
+
 app.use(cors());
 app.options('*', cors());
+
+// app.use(function(req, res, next) {
+//   res.header('Access-Control-Allow-Origin', 'https://around.nomoreparties.co');
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//   res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+//   next();
+// });
 
 // Still working on getting this up
 // app.set('trust proxy', 1);
